5176f5959337ff9c8d82b6a0870b6793c755e38b
Videlin Donchev <ya.5164646777752015@yandex.ru>
2016-05-12 17:49:25 +0300

    100/100 Fix

--- 04.FastАndFurious/FastАndFurious.cs
+++ 04.FastАndFurious/FastАndFurious.cs
@@ -2,21 +2,69 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public static class FastАndFurious
     {
-        private static double[,] bestRoads;
-
         public static void Main()
         {
-            // Read roads
-            Console.ReadLine(); // wtf?
-
-            var roadsList = new List<string[]>();
-
-            var roadIndex = 0;
             var roadIndexes = new Dictionary<string, int>();
+            var roadsMap = GetRoads(roadIndexes);
+            FindAllFastestRoads(roadsMap);
 
+            var drivers = new Dictionary<string, List<Record>>();
+            var speeders = new SortedSet<string>();
+            ProcessRecords(drivers, roadIndexes, roadsMap, speeders);
+
+            speeders.ToList().ForEach(Console.WriteLine);
+        }
+
+        private static void ProcessRecords(Dictionary<string, List<Record>> drivers, Dictionary<string, int> roadIndexes, double[,] roadsMap, SortedSet<string> speeders)
+        {
+            var input = Console.ReadLine();
+            while (input != "End")
+            {
+                var tokens = input.Split(' ');
+                var city = tokens[0];
+                var plate = tokens[1];
+                var time = TimeSpan.Parse(tokens[2]);
+
+                if (!drivers.ContainsKey(plate))
+                {
+                    drivers[plate] = new List<Record>();
+                }
+
+                foreach (var driver in drivers[plate])
+                {
+                    var endTime = driver.Time;
+                    var endCity = driver.City;
+
+                    var roadTime = Math.Abs((endTime - time).TotalHours);
+
+                    var startCityIndex = roadIndexes[city];
+                    var endCityIndex = roadIndexes[endCity];
+
+                    var distanceTime = roadsMap[startCityIndex, endCityIndex];
+
+                    if (!double.IsPositiveInfinity(distanceTime) &&
+                        distanceTime > roadTime)
+                    {
+                        speeders.Add(plate);
+                        break;
+                    }
+                }
+
+                drivers[plate].Add(new Record(time, city));
+                input = Console.ReadLine();
+            }
+        }
+
+        private static double[,] GetRoads(Dictionary<string, int> roadIndexes)
+        {
+            var roadsList = new List<string[]>();
+            var roadIndex = 0;
+
+            Console.ReadLine();
             var input = Console.ReadLine();
             while (input != "Records:")
             {
@@ -41,164 +89,65 @@
 
             var totalRoads = roadIndexes.Count;
             var roadsMap = new double[totalRoads, totalRoads];
+            for (var i = 0; i < roadsMap.GetLength(0); i++)
+            {
+                for (var j = 0; j < roadsMap.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    roadsMap[i, j] = double.PositiveInfinity;
+                }
+            }
 
             foreach (var road in roadsList)
             {
                 var startTown = road[0];
                 var endTown = road[1];
+
                 var roadTime = double.Parse(road[2]) / double.Parse(road[3]);
 
                 var startTownIndex = roadIndexes[startTown];
                 var endTownIndex = roadIndexes[endTown];
+
                 roadsMap[startTownIndex, endTownIndex] = roadTime;
                 roadsMap[endTownIndex, startTownIndex] = roadTime;
             }
 
-            // read records
-            var drivers = new Dictionary<string, List<Position>>();
-            input = Console.ReadLine();
-            while (input != "End")
-            {
-                var tokens = input.Split(' ');
-
-                var location = tokens[0];
-                var plateNUmber = tokens[1];
-                var time = TimeSpan.Parse(tokens[2]);
-
-                if (!drivers.ContainsKey(plateNUmber))
-                {
-                    drivers[plateNUmber] = new List<Position>();
-                }
-
-                drivers[plateNUmber].Add(new Position(time, location));
-
-                input = Console.ReadLine();
-            }
-
-            bestRoads = new double[totalRoads, totalRoads];
-
-            var speeders = new SortedSet<string>();
-            foreach (var driver in drivers)
-            {
-                driver.Value.Sort();
-                for (int i = 0; i < driver.Value.Count - 1; i++)
-                {
-                    var startTime = driver.Value[i].Time;
-                    var endTime = driver.Value[i + 1].Time;
-
-                    var roadTime = (endTime - startTime).TotalHours;
-
-                    var startCity = driver.Value[i].City;
-                    var endCity = driver.Value[i + 1].City;
-
-                    var startCityIndex = roadIndexes[startCity];
-                    var endCityIndex = roadIndexes[endCity];
-
-                    FindFastestRoad(roadsMap, startCityIndex, endCityIndex);
-
-                    var distanceTime = bestRoads[startCityIndex, endCityIndex];
-                    if (distanceTime > roadTime)
-                    {
-                        speeders.Add(driver.Key);
-                    }
-                }
-            }
-
-            foreach (var speeder in speeders)
-            {
-                Console.WriteLine(speeder);
-            }
+            return roadsMap;
         }
 
-        private static void FindFastestRoad(double[,] roadsMap, int startTown, int endTown)
+        private static void FindAllFastestRoads(double[,] roadsMap)
         {
-            var n = roadsMap.GetLength(0);
-
-            var distance = new double[n];
-            for (int i = 0; i < n; i++)
+            var v = roadsMap.GetLength(0);
+            for (var k = 0; k < v; k++)
             {
-                distance[i] = double.MaxValue;
-            }
-
-            distance[startTown] = 0;
-
-            var used = new bool[n];
-            var previous = new int?[n];
-
-            while (true)
-            {
-                double minDistance = double.MaxValue;
-                var minNode = 0;
-                for (int i = 0; i < n; i++)
+                for (var i = 0; i < v; i++)
                 {
-                    if (!used[i] && minDistance > distance[i])
+                    for (var j = 0; j < v; j++)
                     {
-                        minDistance = distance[i];
-                        minNode = i;
-                    }
-                }
-
-                if (minDistance == double.MaxValue)
-                {
-                    break;
-                }
-
-                used[minNode] = true;
-
-                for (int i = 0; i < n; i++)
-                {
-                    if (roadsMap[minNode, i] > 0)
-                    {
-                        var shortestToMinNode = distance[minNode];
-                        var distanceToNextNode = roadsMap[minNode, i];
-
-                        var totalDistance = shortestToMinNode + distanceToNextNode;
-
-                        if (totalDistance < distance[i])
+                        if (roadsMap[i, k] + roadsMap[k, j] < roadsMap[i, j])
                         {
-                            distance[i] = totalDistance;
-                            previous[i] = minNode;
+                            roadsMap[i, j] = roadsMap[i, k] + roadsMap[k, j];
                         }
                     }
                 }
             }
-
-            if (distance[endTown] != double.MaxValue)
-            {
-                bestRoads[startTown, endTown] = distance[endTown];
-                bestRoads[endTown, startTown] = distance[endTown];
-            }
         }
 
-        private static void DevPrinter(double[,] roads)
+        private class Record
         {
-            for (int row = 0; row < roads.GetLength(0); row++)
-            {
-                for (int col = 0; col < roads.GetLength(1); col++)
-                {
-                    Console.Write($"{roads[row, col],-8:f3}");
-                }
-
-                Console.WriteLine();
-            }
-        }
-
-        internal class Position : IComparable<Position>
-        {
-            public Position(TimeSpan time, string city)
+            public Record(TimeSpan time, string city)
             {
                 this.Time = time;
                 this.City = city;
             }
 
-            public TimeSpan Time { get; set; }
+            public TimeSpan Time { get; private set; }
 
-            public string City { get; set; }
-
-            public int CompareTo(Position other)
-            {
-                return this.Time.CompareTo(other.Time);
-            }
+            public string City { get; private set; }
         }
     }
 }
